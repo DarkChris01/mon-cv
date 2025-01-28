1c9d4279dba79566ee4d55a573d075353c8219a3
Bapes Yop Christian <christianbapes@gmail.com>
2025-01-28 17:29:45 +0100

    v0.0.5

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -3,18 +3,17 @@ export default function Experience() {
         <div>
             <h2 className="border-b border-gray-200 mb-3"> Experiences</h2>
             <div>
-                <div className="flex justify-between items-center bg-gray-600 text-white p-1 rounded-xs">
-                    <span>
+                <div className="lg:flex justify-between items-center bg-gray-600 text-white p-1 rounded-xs">
+                    <div>
                         Développeur Web Full Stack
-                    </span>
-                    <span>
-                       De septembre 2024 - janvier 2025
-                    </span>
+                    </div>
+                    <div className="text-end">
+                       septembre 2024 - janvier 2025
+                    </div>
                 </div>
-                <p>
+                <p className="font-semibold my-1">
                     Freelance , Douala CAMEROUN
                 </p>
-
 
                 <ul>
                     <li>Développé et maintenu une plateforme de gestion RH pour plus de 10 000 utilisateurs en utilisant Laravel et Inertia.js.</li>
