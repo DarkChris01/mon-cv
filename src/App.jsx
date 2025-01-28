1c5c76ec1437fa77840a64a2f455dd50f2446186
Bapes Yop Christian <christianbapes@gmail.com>
2025-01-28 22:12:37 +0100

    fixed responsive error

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,24 @@ function App() {
 
   return (
     <>
-      <section className='lg:grid p-2 grid-cols-4 gap-4'>
-        <div className='lg:col-span-1 border-r text-center border-gray-200 h-full py-6 px-2'>
+      <section className='grid lg:grid-cols-4 grid-cols-1 lg:gap-4'>
+        <div className='lg:col-span-1 lg:border-r h-screen lg:h-fit text-center border-gray-200  py-6 px-2'>
           <section id='information'>
             <PersonalInfo />
           </section>
-          <section className='skills group my-4 p-3 bg-gray-50 shadow-lg rounded border border-gray-200/80'>
+          <section className='skills group my-8 p-3 bg-gray-50 shadow-lg rounded border border-gray-200/80'>
             <Skills />
           </section>
         </div>
-        <div className="lg:col-span-3 p-6">
-          <section className='about my-4'>
+        <div className="col-span-3 p-2">
+          <section className='about my-8'>
             <About />
           </section>
 
-          <section className="experience my-4">
+          <section className="experience my-8">
             <Experience />
           </section>
-          <section className="experience my-4">
+          <section className="experience my-8">
             <Project />
           </section>
         </div>
