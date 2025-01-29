3155cc514a439f6413764d19fd6df14d9585d6cd
Bapes Yop Christian <christianbapes@gmail.com>
2025-01-29 11:36:46 +0100

    fixed progress bar loop error

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,23 +1,38 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function ProgressBar({ skill }) {
 
-    const [level, setLevel] = useState(0);
-    let progressBarAnimation;
+    const [progress, setProgress] = useState(0);
+
     useEffect(() => {
+
+        let progressBarAnimation;
+
         function animation() {
-            if (level < skill.level) {
-                setLevel(level + 1)
-                progressBarAnimation = requestAnimationFrame(animation)
-            }
+
+            setProgress(prev => {
+                if (prev < skill.level+1) {
+                    progressBarAnimation = requestAnimationFrame(animation)
+                    return Math.min(prev + 6, skill.level+1); // Augmente en limitant à `skill.level`
+                }
+                cancelAnimationFrame(progressBarAnimation);
+                return prev
+            })
         }
         animation();
+
         return () => cancelAnimationFrame(progressBarAnimation);
-    }, [level])
+    }, [skill])
 
 
     return (
         <>
-            <progress className="border rounded-full overflow-hidden bg-gray-200 w-full h-5" value={level} max="100">{level}%</progress>
+        {/* <div className="w-full bg-gray-200 rounded-full overflow-hidden h-6">
+            <div
+                className="h-full bg-blue-500 transition-all duration-75"
+                style={{ width: `${progress}%` }}
+            />
+        </div> */}
+            <progress className="border rounded-full overflow-hidden bg-gray-200 w-full h-5" value={progress} max="100">{progress}%</progress>
         </>)
 }
